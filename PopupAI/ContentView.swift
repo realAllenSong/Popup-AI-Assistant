40d6eba72b7174ba2e307414d5c7f7e93c08f257
realAllenSong <459566321@qq.com>
2025-06-21 21:37:10 +0800

    "first commit"

---
+++ PopupAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PopupAI
+//
+//  Created by Song Allen on 2025/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PopupAI/PopupAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopupAIApp.swift
+//  PopupAI
+//
+//  Created by Song Allen on 2025/6/21.
+//
+
+import SwiftUI
+
+@main
+struct PopupAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
